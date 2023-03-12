76434627b87db59fda1cb0952cd92047b4fb19fc
Robin Thomas <mail2robbins@gmail.com>
2023-03-12 11:03:14 -0400

    adsense fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,8 +11,34 @@ const Home = () => {
     <>
       <Navbar />
       <Hero />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7865735760405609"
+        crossorigin="anonymous"
+      ></script>
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-7865735760405609"
+        data-ad-slot="3615514016"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+      <noscript>You need to enable JavaScript to run this app.</noscript>
       <About />
       <Services />
+
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-7865735760405609"
+        data-ad-slot="3615514016"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+      <noscript>You need to enable JavaScript to run this app.</noscript>
       <Tours />
       <Footer />
     </>
